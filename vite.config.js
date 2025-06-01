bf3918b61c74e7b37341bc625a997fbb3917413d
cullenwebber <139840023+cullenwebber@users.noreply.github.com>
2025-06-01 17:48:53 +0800

    Push

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/epigroup-orb/",
+  base: "/audio-reactive/",
   publicDir: "public",
   root: "./",
   build: {
